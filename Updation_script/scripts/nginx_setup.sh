eead8da3466bd61e7538317851b4454444c13d3d
Divya Sodha <divyasodha10@gmail.com>
2019-12-25 13:38:54 +0530

    Update nginx_setup.sh

--- Updation_script/scripts/nginx_setup.sh
+++ Updation_script/scripts/nginx_setup.sh
@@ -9,11 +9,11 @@ if [ $? -eq 0 ]; then
 
     sudo service nginx stop 2>> /home/kolibri/Desktop/Updation_script/updation.log
     echo -ne '\n' | sudo add-apt-repository ppa:nginx/stable 2>> /home/kolibri/Desktop/Updation_script/updation.log
-    rm -rf /etc/nginx/sites-enabled/default 2>> /home/kolibri/Desktop/Updation_script/updation.log
-    cp /home/kolibri/Desktop/Updation_script/static_files/default /etc/nginx/sites-enabled/ 2>> /home/kolibri/Desktop/Updation_script/updation.log
+    sudo rm -rf /etc/nginx/sites-enabled/default 2>> /home/kolibri/Desktop/Updation_script/updation.log
+    sudo cp /home/kolibri/Desktop/Updation_script/static_files/default /etc/nginx/sites-enabled/ 2>> /home/kolibri/Desktop/Updation_script/updation.log
 
-    rm -rf /etc/nginx/sites-available/default 2>> /home/kolibri/Desktop/Updation_script/updation.log
-    cp /home/kolibri/Desktop/Updation_script/static_files/default /etc/nginx/sites-available/ 2>> /home/kolibri/Desktop/Updation_script/updation.log
+    sudo rm -rf /etc/nginx/sites-available/default 2>> /home/kolibri/Desktop/Updation_script/updation.log
+    sudo cp /home/kolibri/Desktop/Updation_script/static_files/default /etc/nginx/sites-available/ 2>> /home/kolibri/Desktop/Updation_script/updation.log
     echo 's'| sudo -S apt-get update 2>> /home/kolibri/Desktop/Updation_script/updation.log
     echo 'Y'| sudo -S apt-get install nginx 2>> /home/kolibri/Desktop/Updation_script/updation.log
     echo "nginx setup done" >> /home/kolibri/Desktop/Updation_script/updation.log
