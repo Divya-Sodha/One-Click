e477586fdd85039ef35160dc0ca204c6b972c60b
Divya Sodha <divyasodha10@gmail.com>
2019-12-26 13:58:56 +0530

    Update setup_hotspot.sh

--- Updation_script/scripts/setup_hotspot.sh
+++ Updation_script/scripts/setup_hotspot.sh
@@ -7,14 +7,15 @@ echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
 notify-send "Setting up hotspot on system"
-(sudo systemctl disable systemd-resolved.service;
-sudo systemctl mask systemd-resolved.service;
+(
+#sudo systemctl disable systemd-resolved.service;
+#sudo systemctl mask systemd-resolved.service;
 echo 's'| sudo -S sudo apt-get update;
 echo 'y'| sudo -S apt-get install dpkg-dev cdbs debhelper dh-autoreconf libev-dev libpcre3-dev pkg-config;
 echo 'y'| sudo -S apt-get -y purge dnsmasq hostapd kolibri-hotspot;
 echo 'y'| sudo -S apt-get install -y dnsmasq git;
-#echo 'y'| sudo -S dpkg -i /home/kolibri/Desktop/Updation_script/kolibri-hotspot/hostapd/hostapd*.deb;
-echo 'y'| sudo apt-get install hostapd;
+echo 'y'| sudo -S dpkg -i /home/kolibri/Desktop/Updation_script/kolibri-hotspot/hostapd/hostapd*.deb;
+#echo 'y'| sudo apt-get install hostapd;
 echo 'y'| sudo -S dpkg -i /home/kolibri/Desktop/Updation_script/kolibri-hotspot/kolibri-hotspot*.deb ) 2>> /home/kolibri/Desktop/Updation_script/updation.log
 echo "Hotspot setup done " >> /home/kolibri/Desktop/Updation_script/updation.log
 zenity --info --text="Hotspot setup Done."
